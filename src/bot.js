ba0a957553505d1f9cd6a542d16008cddb581e12
payalpagaria <pagariapayal67@gmail.com>
2023-01-06 22:55:18 +0530

    update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 // client class allows us to interact with dicord api
 const { Client, GatewayIntentBits } = require('discord.js');
-
+const PREFIX="$"
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -16,9 +16,21 @@ client.on("ready",()=>{
 })
 client.on("messageCreate",(message)=>{
 	if(message.author.bot) return;
-	console.log(`${message.author.tag} => ${message.content}`);
-	if(message.content==="hey"){
-		message.channel.send("hello there")
-	}
+	if(message.content.startsWith(PREFIX)){
+			const [CMD_NAME,...args]=message.
+		content.trim().
+		substring(PREFIX.length).
+		split(/\s+/); //match all white spaces
+	
+		if(CMD_NAME==='kick'){
+			const member =message.guild.
+			message.channel.send("kicked the user");
+		}
+		else if(CMD_NAME==='ban'){
+			message.channel.send("Banned the user");
+
+		}
+	}	
 })
+
 client.login(process.env.DISCORDJS_BOT_TOKEN)
